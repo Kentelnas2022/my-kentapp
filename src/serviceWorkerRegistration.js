1d4f9dc8e24982d6ca5b37f1dec093ec936f774a
Kentelnas2022 <kentzorelelnas.202200472@gmail.com>
2024-12-14 16:14:49 +0800

    First commit

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,58 @@
+// This file is for service worker registration
+// Automatically registers the service worker and enables offline functionality
+// Add more functionality like `unregister()` as needed
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname === '127.0.0.1'
+);
+
+export function register() {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl);
+      } else {
+        registerValidSW(swUrl);
+      }
+    });
+  }
+}
+
+function registerValidSW(swUrl) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then((registration) => {
+      console.log('Service Worker registration successful with scope: ', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed: ', error);
+    });
+}
+
+function checkValidServiceWorker(swUrl) {
+  fetch(swUrl)
+    .then((response) => {
+      if (
+        response.status === 404 ||
+        response.headers.get('content-type').indexOf('javascript') === -1
+      ) {
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.unregister();
+        });
+      } else {
+        registerValidSW(swUrl);
+      }
+    })
+    .catch(() => {
+      console.log('No internet connection found. App is running in offline mode.');
+    });
+}
